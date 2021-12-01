bce845fda9f7e8a8b273f3974c0639b508309a00
Ilze Rigerte <irigerte@gmail.com>
2021-12-01 20:05:40 +0200

    Connecting Spring app with DB, mapping entities with DB, different JPA annotations

---
+++ src/main/java/com/example/sprdatajpa/SprDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprdatajpa/SprDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
